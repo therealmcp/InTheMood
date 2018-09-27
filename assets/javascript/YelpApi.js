7ea3706632fe596c2deca894bd16085fd2194c1b
Mona <monarahmanimehr@gmail.com>
2018-09-26 19:43:23 -0700

    yelp Api

---
+++ assets/javascript/YelpApi.js
@@ -0,0 +1,13 @@
+var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=seattle";
+
+$.ajax({
+    url: myurl,
+    headers: {
+     'Authorization':'Bearer gdCapxGnUhlNcpxkkNXxQtkDAfCB8ZjcBDyQ0ZiiY5Ia9xqObyUDHnNgj9L8Kqlit6tBsPcA25nKBpEH9boKwgMwxCvl26f_ZZpVapfnD9B8MxGd5H6bG6OwsQypW3Yx',
+ },
+    method: 'GET',
+    dataType: 'json',
+    success: function(data){
+        console.log(data);
+    }
+ });      
