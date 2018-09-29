b933482f6ecb4f33bcf475f7b5bb2c23ef966606
unknown <cp.mcpike@gmail.com>
2018-09-29 12:06:09 -0700

    google.js location should be working

--- assets/javascript/appGoogleReviews.js
+++ assets/javascript/appGoogleReviews.js
@@ -1,6 +1,9 @@
+var place
+
 $(document).ready(function() {
 
-    var x = document.getElementById("demo");
+    // var x = document.getElementById("demo");
+
 
     function getLocation() {
         if (navigator.geolocation) {
@@ -12,35 +15,39 @@ $(document).ready(function() {
     
     getLocation();
 
-    function showPosition() {
+    console.log(place);
+
+    function showPosition(position) {
         position.coords.latitude + 
         position.coords.longitude;
         var location = position.coords.latitude + "," + position.coords.longitude;
-        return location;
+        place = location;
+        console.log(position);
+        // return location;
         }
 
-    location = showPosition();
+    // location = showPosition();
 
     console.log("location will be " + location);
 
-$("#submit").on("click", function(event) {
+    $("#submit").on("click", function(event) {
 
-var apiKey = "AIzaSyBolUOu_G0aNYs7L3-byaAek4lJmDE3BV8";
-var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + location + "&radius=500&types=food&key=" + apiKey;
-// var queryURL = "https://cors-anywhere.herokuapp.com/https://www.google.com/maps/search/json?api=1&q=90210"
-var queryParams = {
-    "api-key": "AIzaSyBolUOu_G0aNYs7L3-byaAek4lJmDE3BV8"
-};
+    var apiKey = "AIzaSyBolUOu_G0aNYs7L3-byaAek4lJmDE3BV8";
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + place + "&radius=500&types=food&key=" + apiKey;
+    // var queryURL = "https://cors-anywhere.herokuapp.com/https://www.google.com/maps/search/json?api=1&q=90210"
+    var queryParams = {
+        "api-key": "AIzaSyBolUOu_G0aNYs7L3-byaAek4lJmDE3BV8"
+    };
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
 
-    console.log(response);
+        console.log(response);
 
-})
+        })
 
-})
+    })
 
 });
