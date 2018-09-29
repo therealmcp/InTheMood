498666260834c372a10bfb014d7d9c5315196037
unknown <cp.mcpike@gmail.com>
2018-09-29 11:26:27 -0700

    changes to index and appGoogleR....js

--- assets/javascript/appGoogleReviews.js
+++ assets/javascript/appGoogleReviews.js
@@ -1,11 +1,36 @@
+$(document).ready(function() {
+
+    var x = document.getElementById("demo");
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else { 
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+    
+    getLocation();
+
+    function showPosition() {
+        position.coords.latitude + 
+        position.coords.longitude;
+        var location = position.coords.latitude + "," + position.coords.longitude;
+        return location;
+        }
+
+    location = showPosition();
+
+    console.log("location will be " + location);
+
+$("#submit").on("click", function(event) {
 
 var apiKey = "AIzaSyBolUOu_G0aNYs7L3-byaAek4lJmDE3BV8";
-var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=47.614502,-122.329566&radius=500&types=food&name=harbour&key=" + apiKey;
+var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + location + "&radius=500&types=food&key=" + apiKey;
+// var queryURL = "https://cors-anywhere.herokuapp.com/https://www.google.com/maps/search/json?api=1&q=90210"
 var queryParams = {
     "api-key": "AIzaSyBolUOu_G0aNYs7L3-byaAek4lJmDE3BV8"
 };
-
-var article = "";
 
 $.ajax({
     url: queryURL,
@@ -16,4 +41,6 @@ $.ajax({
 
 })
 
+})
 
+});
