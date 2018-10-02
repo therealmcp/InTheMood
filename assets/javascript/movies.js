4e6b3f30ff66a814f90d5e82ab1e06c9d55f876e
Andrii <andrg@outlook.com>
2018-10-01 21:57:04 -0700

    movies update 2

--- assets/javascript/movies.js
+++ assets/javascript/movies.js
@@ -1,10 +1,14 @@
            // construct the url with parameter values
-           var apikey = "wcynn7m8cmtd5c24zfer2f77";
+           var apikey = "png3faunycpgqmqspytjkzxe";
            var baseUrl = "http://data.tmsapi.com/v1.1";
            var showtimesUrl = baseUrl + '/movies/showings';
            var zipCode = {};
            var d = new Date();
            var today = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate();
+          
+           var movieArray = [];
+           var comedy = [];
+           var chosenSpot;
 
 
             $("#find-restaurant").on("click", function(event) {
@@ -30,6 +34,8 @@
 
              data.forEach(function(movie){    
                 
+                console.log(movie);
+
                 var tBody = $("tbody");
                 var tRow = $("<tr>");
                 var titleTd = $("<td>").text(movie.title);
@@ -54,19 +60,54 @@
                 tRow.append(titleTd, yearTd, showtimeTd, theatreTd);
                 tBody.append(tRow);
 
-                            console.log(movie.title);
-                            console.log(movie.shortDescription);
-                            console.log(movie.showtimes[0].dateTime);
-                            console.log(movie.showtimes[0].theatre.name);
-                            console.log(movie.name);
-                            console.log(movietimes);
-                            console.log(locations);
-                        })   
-                        console.log(data);
+                        //    console.log(movie.title);
+                        //    console.log(movie.shortDescription);
+                        //    console.log(movie.showtimes[0].dateTime);
+                        //    console.log(movie.showtimes[0].theatre.name);
+                        //    console.log(movie.genres);
+                        //    console.log(movie.name);
+                        //    console.log(movietimes);
+                        //    console.log(locations);
+                            
+                        
+                         // console.log(data);
                         $("#hide-me").show();
+
+                        // new code
+                        
+                        console.log(movie.genres.indexOf('Comedy'));
+                        if (movie.genres.indexOf('Comedy') != -1) {
+                            comedy.push(movie);
+                        }
+                        console.log(comedy);
+
+
+
+                        // for (i = 0; i < movie.length; i++) {
+                            
+                        //    // movieArray.push(movie[i]);
+                        //     // console.log("resutls are ", response.results[i]);
+                        //     // if (movieArray[i].genres === "Comedy") {
+                        //     //     comedy.push(movieArray[i]);
+                        //     // } 
+                        // }
+                        //console.log(Comedy);
+                    })
+                        
+                        // function selected(array) {
+                        //     var chosenMood = $("#mood").val();
+                        //     console.log("you chose ", chosenMood);
+                        //     chosenSpot = array[Math.floor(Math.random()*array.length)]
+                        //     $("#dinner").prepend(chosenSpot.icon);
+                        //     };
+                
+                        // selected(comedy);
+
+
+
+
         };
 
         // Step 1. ConsoleLog Genre
         // Step 2. Explore .val method
         // Step 3. Check API calls
-        // Step 4. 
